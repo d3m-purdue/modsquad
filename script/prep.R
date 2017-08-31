762f3ab65f92948c0116ac3497aa06aa654f0f48
Roni Choudhury <roni.choudhury@kitware.com>
2017-08-31 09:53:37 -0400

    Restructure package.json and shell.nix to work everywhere

--- script/prep.R
+++ script/prep.R
@@ -1,2 +1,5 @@
 library(devtools)
-devtools::install_github('d3m-purdue/d3mLm')
+library(withr)
+
+options(unzip="internal")
+withr::with_libpaths(new="./rlib", code=devtools::install_github('d3m-purdue/d3mLm'))
---
+++ shell.nix
@@ -0,0 +1,36 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+  name = "d3mEnv";
+  buildInputs = [
+    cairo
+    icu
+    lzma
+    pcre
+    readline
+
+    python27Full
+    python27Packages.virtualenv
+    python27Packages.pip
+
+    R
+    rPackages.devtools
+    rPackages.withr
+  ];
+  src = null;
+  shellHook = ''
+    # Allow the use of wheels.
+    SOURCE_DATE_EPOCH=$(date +%s)
+
+    # Add the virtualenv path to the shell path.
+    mkdir -p venv
+    export PATH=$PWD/venv/bin:$PATH
+
+    # Add the local R libraries to the R library path.
+    mkdir -p rlib
+    export R_LIBS_SITE=$PWD/rlib:$R_LIBS_SITE
+
+    # Put readline in the LD_LIBRARY_PATH.
+    export LD_LIBRARY_PATH=${readline}/lib:$LD_LIBRARY_PATH
+  '';
+}
